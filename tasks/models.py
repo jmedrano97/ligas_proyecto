53b0080b4e1140d93dd334dc9ceef1114dbbde2b
Jaime Eduardo Medrano Bernales <jaime.medrano@htatec.com>
2023-04-04 00:03:11 -0600

    ya guarda tareas

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.user)
+    
+
+
+
+
+# from django.db import models
+
+# class Equipo(models.Model):
+#     nombre = models.CharField(max_length=50)
+#     ciudad = models.CharField(max_length=50)
+
+#     def __str__(self):
+#         return self.nombre
+
+# class Jugador(models.Model):
+#     nombre = models.CharField(max_length=50)
+#     fecha_nacimiento = models.DateField()
+#     equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.nombre
+
+# class Partido(models.Model):
+#     fecha = models.DateField()
+#     hora = models.TimeField()
+#     estadio = models.CharField(max_length=50)
+#     equipo_local = models.ForeignKey(Equipo, related_name='partidos_local', on_delete=models.CASCADE)
+#     equipo_visitante = models.ForeignKey(Equipo, related_name='partidos_visitante', on_delete=models.CASCADE)
+#     goles_local = models.PositiveIntegerField()
+#     goles_visitante = models.PositiveIntegerField()
+
+#     def __str__(self):
+#         return f"{self.equipo_local} vs. {self.equipo_visitante}"
+
+# class Clasificacion(models.Model):
+#     equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+#     posicion = models.PositiveIntegerField()
+#     puntos = models.PositiveIntegerField()
+#     partidos_jugados = models.PositiveIntegerField()
+#     partidos_ganados = models.PositiveIntegerField()
+#     partidos_empatados = models.PositiveIntegerField()
+#     partidos_perdidos = models.PositiveIntegerField()
+#     goles_favor = models.PositiveIntegerField()
+#     goles_contra = models.PositiveIntegerField()
+
+#     def __str__(self):
+#         return f"{self.equipo} - Posición {self.posicion}"
+
+# class EstadisticasJugador(models.Model):
+#     jugador = models.ForeignKey(Jugador, on_delete=models.CASCADE)
+#     goles = models.PositiveIntegerField()
+#     asistencias = models.PositiveIntegerField()
+#     tarjetas_amarillas = models.PositiveIntegerField()
+#     tarjetas_rojas = models.PositiveIntegerField()
+
+#     def __str__(self):
+#         return f"Estadísticas de {self.jugador}"
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 from django.db import IntegrityError
 
+from tasks.forms import TaskForm
+from .models import Task
 
 # Create your views here.
 
@@ -37,4 +39,47 @@ def signup(request):
         })
 
 def tasks(request):
-    return render(request, 'tasks.html')
+    # tasks = Task.objects.filter(user=request.user)
+    tasks = Task.objects.all()
+    return render(request, 'tasks.html', {'tasks': tasks})
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+            'form': TaskForm()
+        })
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'create_task.html', {
+                'form': TaskForm(),
+                'error': 'Bad data passed in. Try again.'
+            })
+
+            
+
+def signout(request):
+    # if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html',{
+            'form': AuthenticationForm()
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST["username"], password=request.POST["password"])
+        if user is None:
+            return render(request, 'signin.html', {
+                'error': 'Username and password did not match'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks') 
