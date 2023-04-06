26f502f0dde5f8f7b8e50d5cb6d06265f444f2ed
Jaime Eduardo Medrano Bernales <jaime.medrano@htatec.com>
2023-04-06 13:42:47 -0600

    modelos de liga

--- miliga/admin.py
+++ miliga/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Liga, Equipo, Jugador, CampoDeJuego, Partido, Clasificacion
 
-# Register your models here.
+@admin.register(Liga)
+class LigaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'ciudad', 'estado', 'pais', 'fecha_inicio', 'fecha_final')
+
+@admin.register(Equipo)
+class EquipoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'campo', 'liga')
+
+@admin.register(Jugador)
+class JugadorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'fecha_nacimiento', 'ocupacion', 'numero', 'equipo', 'liga')
+    list_filter = ('ocupacion', 'equipo', 'liga')
+
+@admin.register(CampoDeJuego)
+class CampoDeJuegoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'ubicacion', 'liga')
+
+@admin.register(Partido)
+class PartidoAdmin(admin.ModelAdmin):
+    list_display = ('fecha', 'hora_inicio', 'hora_terminacion', 'campo_juego', 'equipo_local', 'equipo_visitante', 'goles_local', 'goles_visitante', 'liga')
+    list_filter = ('fecha', 'campo_juego', 'liga')
+
+@admin.register(Clasificacion)
+class ClasificacionAdmin(admin.ModelAdmin):
+    list_display = ('equipo', 'puntuacion', 'posicion', 'goles_a_favor', 'goles_en_contra', 'diferencia_goles', 'liga')
+    list_filter = ('liga',)
--- miliga/models.py
+++ miliga/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+class Liga(models.Model):
+    nombre = models.CharField(max_length=100)
+    ciudad = models.CharField(max_length=100)
+    estado = models.CharField(max_length=100)
+    pais = models.CharField(max_length=100)
+    fecha_inicio = models.DateField()
+    fecha_final = models.DateField()
+
+class Equipo(models.Model):
+    nombre = models.CharField(max_length=100)
+    campo = models.CharField(max_length=100, blank=True, null=True, default=None)
+    liga = models.ForeignKey(Liga, on_delete=models.CASCADE)
+
+class Jugador(models.Model):
+    # escribir opciones de posicion
+    ocupacion = (
+        ('Jugador', 'Jugador'),
+        ('Director tecnico', 'Director tecnico'),
+        ('Auxiliar', 'Auxiliar'),
+    )
+    nombre = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField()
+    ocupacion = models.CharField(max_length=100,null=True, blank=True, choices=ocupacion)
+    numero = models.IntegerField(null=True, blank=True)
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    liga = models.ForeignKey(Liga, on_delete=models.CASCADE)
+
+class CampoDeJuego(models.Model):
+    nombre = models.CharField(max_length=100)
+    ubicacion = models.CharField(max_length=100)
+    liga = models.ForeignKey(Liga, on_delete=models.CASCADE)
+
+class Partido(models.Model):
+    fecha = models.DateField()
+    hora_inicio = models.TimeField()
+    hora_terminacion = models.TimeField()
+    campo_juego = models.ForeignKey(CampoDeJuego, on_delete=models.CASCADE)
+    equipo_local = models.ForeignKey(Equipo, on_delete=models.CASCADE, related_name='partidos_local')
+    equipo_visitante = models.ForeignKey(Equipo, on_delete=models.CASCADE, related_name='partidos_visitante')
+    goles_local = models.PositiveIntegerField()
+    goles_visitante = models.PositiveIntegerField()
+    liga = models.ForeignKey(Liga, on_delete=models.CASCADE)
+    
+    class Meta:
+        unique_together = ('fecha', 'hora_inicio', 'campo_juego')
+
+class Clasificacion(models.Model):
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    puntuacion = models.IntegerField()
+    posicion = models.IntegerField()
+    goles_a_favor = models.IntegerField()
+    goles_en_contra = models.IntegerField()
+    diferencia_goles = models.IntegerField()
+    liga = models.ForeignKey(Liga, on_delete=models.CASCADE)
+
